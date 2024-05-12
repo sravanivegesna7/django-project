6d589d6043f13eb65f0409fa6a9fe2fe6641637f
sravanivegesna7 <sravani.vegesna@itversity.com>
2024-05-12 06:25:24 +0530

    created itv_blog application

---
+++ itv_blog/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
